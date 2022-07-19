b3fce16ff30a088f7b35506478a5649fec3aba0d
Eduard Marian <55080847+eduardxyz@users.noreply.github.com>
2022-07-19 21:25:39 +0300

    Fix comsetics (#8)
    * Refactors:
    
    - LYX changed for LYXt
    - HTML descriptions
    
    Removes:
    
    - Facebook example
    
    * Refactors Twitter request message from #Ethereum to #Lukso
    
    * Capitalize #Lukso to #LUSKO

--- faucet.go
+++ faucet.go
@@ -109,13 +109,13 @@ func main() {
 	// Construct the payout tiers
 	amounts := make([]string, *tiersFlag)
 	periods := make([]string, *tiersFlag)
-	amounts[0] = fmt.Sprintf("%s LYX", strconv.FormatFloat(10, 'f', -1, 64))
+	amounts[0] = fmt.Sprintf("%s LYXt", strconv.FormatFloat(10, 'f', -1, 64))
 	periods[0] = fmt.Sprintf("%d h", 1)
 
-	amounts[1] = fmt.Sprintf("%s LYX", strconv.FormatFloat(50, 'f', -1, 64))
+	amounts[1] = fmt.Sprintf("%s LYXt", strconv.FormatFloat(50, 'f', -1, 64))
 	periods[1] = fmt.Sprintf("%d h", 3)
 
-	amounts[2] = fmt.Sprintf("%s LYX", strconv.FormatFloat(221, 'f', -1, 64))
+	amounts[2] = fmt.Sprintf("%s LYXt", strconv.FormatFloat(221, 'f', -1, 64))
 	periods[2] = fmt.Sprintf("%d days", 4)
 
 	//for i := 0; i < *tiersFlag; i++ {
